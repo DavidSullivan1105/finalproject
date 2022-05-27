e551c11624f76881dde8f67cafc08110b64d0a0b
David Sullivan <dcsulliv@gmail.com>
2022-05-26 21:22:28 -0400

    9:21 Thursday GreenPlan start

---
+++ GreenPlan.repository/Car.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GreenPlan.repository
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public Color ColorOptions { get; set; }
+
+        public decimal MPG { get; set; }
+
+        public BodyStyle BodyStyleOptions { get; set; }
+
+        public Transmission TransmissionOptions { get; set; }
+
+        public int Cylinders { get; set; }
+
+        public decimal Price { get; set; }
+
+        public Drivetrain DrivetrainOptions { get; set; }
+
+        public FuelType FuelTypeOptions { get; set; }
+
+        public Car (string make, string model, int year, Color color, decimal MPG, BodyStyle bodyStyle, Transmission transmission, int cylinders, decimal price, Drivetrain drivetrain, FuelType fueltype )
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            ColorOptions = color;
+            MPG = MPG;
+            BodyStyleOptions = bodyStyle;
+            TransmissionOptions = transmission;
+            Cylinders = cylinders;
+            Price = price;
+            DrivetrainOptions = drivetrain;
+            FuelTypeOptions = fueltype;
+
+        }
+
+        public Car (){}
+
+
+
+
+
+    } 
+    public enum Color { Red, Blue, Black, White, Yellow, Green, Silver, Tan} 
+    public enum BodyStyle { CargoVan, Convertible, Coupe, Hatchback, Minivan, PassengerVan, PickupTruck, SUV, Sedan, Wagon}
+    public enum Transmission { automatic, manual } 
+    public enum Drivetrain { AllWheelDrive, FourWheelDrive, FrontWheelDrive, RearWheelDrive }
+    public enum FuelType { Electric, Gasoline, Hybrid }
+
+}
--- GreenPlan.repository/GreenPlanRepository.cs
+++ GreenPlan.repository/GreenPlanRepository.cs
@@ -7,6 +7,31 @@ namespace GreenPlan.repository
 {
     public class GreenPlanRepository
     {
-        
+        //DB
+
+
+
+        //Create
+
+
+
+        //Read
+
+
+
+        //Update
+
+
+
+        //Delete
+
+
+
+        //Seed Data Method
+
+        public void SeedCarData()
+        {
+            Car = new Car ("Ford", "MaverickXLT", 2022, Color.Silver, 29, BodyStyle.PickupTruck, Transmission.automatic, 4, 36944, Drivetrain.AllWheelDrive, FuelType.Gasoline );
+        }
     }
 }
